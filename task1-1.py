eb4f2f4566001a2409a18e1f9b51059201f60beb
unknown <weng0989969828@gmail.com>
2022-04-19 14:34:42 +0800

    binary template [blues]: stft = 0.17, cqt = 0.15, cens = 0.17

--- task1-1.py
+++ task1-1.py
@@ -1,4 +1,5 @@
 import librosa
+import mir_eval
 import scipy
 import numpy as np
 from os import listdir
@@ -6,6 +7,8 @@ from os import listdir
 genres = ["blues", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
 minor = {0: 9, 1: 10, 2: 11, 3: 0, 4: 1, 5: 2, 6: 3, 7: 4, 8: 5, 9: 6, 10: 7, 11: 8}
 major = {0: 3, 1: 4, 2: 5, 3: 6, 4: 7, 5: 8, 6: 9, 7: 10, 8: 11, 9: 0, 10: 1, 11: 2}
+tonic = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'major', 'minor']
+eval_tonic = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'major', 'minor']
 
 #   0   1  2   3  4  5   6  7   8  9  10  11
 # [ C, C#, D, D#, E, F, F#, G, G#, A, A#, B ]
@@ -15,37 +18,58 @@ binary_template = [[1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1], [1, 1, 0, 1, 0, 1, 1, 0
                    [0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1], [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
                    [1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0], [0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
                    [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0], [0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1]]
+
+scores_stft = []
+scores_cqt = []
+scores_cens = []
+
 for g in genres:
+
     folder_path = "GTZAN/wav/" + g
     files = listdir(folder_path)
-    result_stft = []
-    result_cqt = []
-    result_cens = []
-    
-    for f in files:
+
+    score_stft, score_cqt, score_cens = 0, 0, 0
+    num_of_file = len(files)
+
+    for file_id, f in enumerate(files):
+
+        print('file:{:03d}'.format(file_id))
+        skip = False
+        ans_str = ''
+        with open('GTZAN/key/{genre}/{genre}.{id:05d}.lerch.txt'.format(genre = g, id = file_id)) as anstxt:
+            ans_tonic = int(anstxt.readline())
+            if ans_tonic not in range(24):
+                skip = True
+                print('skip')
+                num_of_file -= 1
+
+            if ans_tonic >= 12:
+                ans_str = eval_tonic[ans_tonic - 12] + ' ' + eval_tonic[-1]
+            else:
+                ans_str = eval_tonic[ans_tonic] + ' ' + eval_tonic[-2]
+            print('ans:', ans_str)
+        
+        if skip:
+            continue
+
         y, sr = librosa.load(folder_path + '/' + f)
         
         feature_stft = librosa.feature.chroma_stft(y = y, sr = sr)
         feature_cqt = librosa.feature.chroma_cqt(y = y, sr = sr)
         feature_cens = librosa.feature.chroma_cens(y = y, sr = sr)
-        
         features = [feature_stft, feature_cqt, feature_cens]
 
         for id,f in enumerate(features):
             
             bin_avg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            num_bin = f.shape[0]
-            num_frame = f.shape[1]
-            
-            max_bin = 0
-            max_r = -1
+            num_bin, num_frame = f.shape[0], f.shape[1]
+            max_bin, max_r = 0, 0
 
             for i in range(num_bin):
                 for energy in f[i]:
                     bin_avg[i] += energy
-                bin_avg[i] = bin_avg[i] / num_frame
+                bin_avg[i] /= num_frame
             
-
             # calculate R
             # find biggest R
             for i,bt in enumerate(binary_template):
@@ -54,27 +78,29 @@ for g in genres:
                     max_r = r
                     max_bin = i
 
-
+            tonic_str = ''
             # check energy of M and m
             if bin_avg[minor[max_bin]] > bin_avg[max_bin]:
-                max_bin = minor[max_bin]
-                max_bin = (max_bin + 3) % 12
-                max_bin += 12
+                tonic_str = tonic[minor[max_bin]] + ' ' + tonic[-1]
             else:
-                max_bin = (max_bin + 3) % 12
+                tonic_str = tonic[max_bin] + ' ' + tonic[-2]
 
             if id == 0:
-                result_stft.append(max_bin)
-                # print('stft:', max_bin)
+                print('stft:', tonic_str, end = ', ')
+                score_stft += mir_eval.key.evaluate(tonic_str, ans_str)['Weighted Score']
             elif id == 1:
-                result_cqt.append(max_bin)
-                # print('cqt:', max_bin)
+                print(' cqt:', tonic_str, end = ', ')
+                score_cqt += mir_eval.key.evaluate(tonic_str, ans_str)['Weighted Score']
             else:
-                result_cens.append(max_bin)
-                # print('cens:', max_bin)
+                print('cens:', tonic_str, end = '')
+                score_cens += mir_eval.key.evaluate(tonic_str, ans_str)['Weighted Score']
+            
+        print()
+        print('score: stft:{}, cqt:{}, cens:{}'.format(score_stft, score_cqt, score_cens))
 
-    print('stft:', result_stft[0:10])
-    print('cqt:', result_cqt[:10])
-    print('cens:', result_cens[:10])
-        
+    scores_stft.append(score_stft/num_of_file)
+    scores_cqt.append(score_cqt/num_of_file)
+    scores_cens.append(score_cens/num_of_file)   
+    print(g, ':')
+    print(scores_stft, scores_cqt, scores_cens)            
     break
