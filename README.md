5ffa80e26ce910aaf8b13a0e8aeb88bb7e370de8
unknown <weng0989969828@gmail.com>
2022-04-21 14:54:51 +0800

    update result of 1-2-audio-harmonic

--- README.md
+++ README.md
@@ -160,10 +160,10 @@ Repeat the process in Q1 on the MIDI data and all the available audio versions (
 
 |Version|stft score|cqt score|cens score|
 |-------|----------|---------|----------|
-| FI66 |  |  |  |
-| FI80 |  |  |  |
-| HU33 |  |  |  |
-| SC06 |  |  |  |
+| FI66 | 0.687500 | 0.862500 | 0.800000 |
+| FI80 | 0.433333 | 0.625000 | 0.687500 |
+| HU33 | 0.708333 | 0.666667 | 0.708333 |
+| SC06 | 0.595833 | 0.695833 | 0.725000 |
 
 ### Q3 (bonus)
 Construct the templates for the 24 major/minor keys using the GiantStep dataset. There are many possible ways to construct the templates. There can also be multiple templates for each key. For example, the template of D major can be constructed by taking the average over all chroma vectors annotated as D major in the dataset. We can also take the 𝑘-means algorithm over these chroma vectors to obtain 𝑘 templates for D major. For the keys not in the dataset, you may consider constructing them by circular shifting from the existing keys. Perform global key finding on the GTZAN dataset using the data-driven template. Does this method benefit some genres? Discuss your results.
---
+++ task1-2-audio-harmonic.py
@@ -0,0 +1,82 @@
+import librosa
+import mir_eval
+import scipy
+import numpy as np
+from os import listdir
+import template
+import csv
+
+versions = ['FI66', 'FI80', 'HU33', 'SC06']
+
+scores_stft = []
+scores_cqt = []
+scores_cens = []
+
+for v_id, v in enumerate(versions):
+
+    print('- [{}]'.format(v))
+    
+    folder_path = "SWD/01_RawData/audio_wav/" + v
+    files = listdir(folder_path)
+
+    score_stft, score_cqt, score_cens = 0, 0, 0
+    num_of_file = len(files)
+
+    ans_list = []
+    with open('SWD/02_Annotations/ann_audio_globalkey_' + v + '.csv') as anscsv:
+        csvreader = csv.reader(anscsv)
+        for row in csvreader:
+            ans_list.append(row[2])
+
+    for file_id, f in enumerate(files):
+   
+        y, sr = librosa.load('{folder}/{file}'.format(folder = folder_path, file = f))
+        # print('file{:02d}: {}'.format(file_id, f))
+        
+        feature_stft = librosa.feature.chroma_stft(y = y, sr = sr)
+        feature_cqt = librosa.feature.chroma_cqt(y = y, sr = sr)
+        feature_cens = librosa.feature.chroma_cens(y = y, sr = sr)
+        features = [feature_stft, feature_cqt, feature_cens]
+
+        for id,f in enumerate(features):
+            
+            bin_avg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            num_bin, num_frame = f.shape[0], f.shape[1]
+            max_bin, max_r = 0, 0
+
+            for i in range(num_bin):
+                for energy in f[i]:
+                    bin_avg[i] += energy
+                bin_avg[i] /= num_frame
+            
+            # calculate R
+            # find biggest R
+            
+            tonic_str = ''
+            for i,ks in enumerate(template.HM_major_template):
+                r = scipy.stats.pearsonr(bin_avg, ks)[0]
+                if r > max_r or i == 0:
+                    max_bin = i
+                    max_r = r
+                    tonic_str = template.tonic[i] + ' ' + template.tonic[-2]
+
+            for i,ks in enumerate(template.HM_minor_template):
+                r = scipy.stats.pearsonr(bin_avg, ks)[0]
+                if r > max_r or (r == max_r and bin_avg[i] > bin_avg[max_bin]):
+                    max_bin = i
+                    max_r = r
+                    tonic_str = template.tonic[i] + ' ' + template.tonic[-1]
+
+            if id == 0:
+                score_stft += mir_eval.key.evaluate(ans_list[file_id], tonic_str)['Weighted Score']
+            elif id == 1:
+                score_cqt += mir_eval.key.evaluate(ans_list[file_id], tonic_str)['Weighted Score']
+            else:
+                score_cens += mir_eval.key.evaluate(ans_list[file_id], tonic_str)['Weighted Score']
+            
+    scores_stft.append(score_stft/num_of_file)
+    scores_cqt.append(score_cqt/num_of_file)
+    scores_cens.append(score_cens/num_of_file)   
+    
+    print('score: stft {}, cqt {}, cens {}'.format(scores_stft[v_id], scores_cqt[v_id], scores_cens[v_id]))
+    
